7dee7035aee5ad0265fe53f755f70d61819a18f5
keisuke <vkzg@gmail.com>
2021-05-04 18:06:07 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
   def new
+    @user = User.new#ユーザーオブジェクトを生成しインスタンス変数に代入
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
